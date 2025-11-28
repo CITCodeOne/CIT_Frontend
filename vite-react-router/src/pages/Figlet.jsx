ec08ac67d7ed86a907a4c6e109593bb3d257800e
pBlem <petersblem@birman.dk>
2025-11-28 15:48:54 +0100

    Figlet implemented

---
+++ vite-react-router/src/pages/Figlet.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import figlet from 'figlet';
+
+// Importing fonts to ensure they are available
+import standard from "figlet/fonts/Standard";
+import roman from "figlet/fonts/Roman";
+// Choose and load a font into standard. figlet will use standard by default
+figlet.parseFont("Standard", roman);
+
+function Figlet() {
+        const { text } = useParams();
+
+        return (
+                <FigletBox text={text || 'Hello, World!'} />
+        );
+}
+
+function FigletBox({ text }) {
+        const [figletText, setFigletText] = React.useState('');
+
+        React.useEffect(() => {
+                figlet.text(
+                        text,
+                        {
+                                font: 'Standard',
+                                horizontalLayout: "default",
+                                verticalLayout: "default",
+                                width: 180,
+                                whitespaceBreak: true,
+                        },
+                        (err, data) => {
+                                if (err) {
+                                        console.error('Figlet error:', err);
+                                        setFigletText('Error generating figlet text.');
+                                        return;
+                                }
+                                setFigletText(data);
+                        });
+        }, [text]);
+
+        return (
+                <pre style={{ fontFamily: 'monospace', whiteSpace: 'pre-wrap' }}>
+                        {figletText}
+                </pre>
+        );
+}
+
+export default Figlet;
--- vite-react-router/src/routes/AppRoutes.jsx
+++ vite-react-router/src/routes/AppRoutes.jsx
@@ -3,15 +3,17 @@ import Home from '../pages/Home';
 import About from '../pages/About';
 import CompAndProps from '../pages/CompAndProps';
 import CustomCarousel from '../pages/CustomCarousel';
+import Figlet from '../pages/Figlet';
 
 function AppRoutes() {
   return (
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/compandprops" element={<CompAndProps />} />
-        <Route path="/customcarousel" element={<CustomCarousel />}/>
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/compandprops" element={<CompAndProps />} />
+      <Route path="/customcarousel" element={<CustomCarousel />} />
+      <Route path="/figlet/:text" element={<Figlet />} />
+    </Routes>
   );
 }
 
