e911dfadcf81090642d188a9b3c106c8f4cbd726
JepMach <jeppea@ruc.dk>
2025-12-08 10:48:10 +0100

    re-added scroll bar to all pages

--- vite-react-router/src/components/Navbar.jsx
+++ vite-react-router/src/components/Navbar.jsx
@@ -96,7 +96,7 @@ export default function NavbarLayout() {
                         />
                         <Container
                                 fluid
-                                className="flex-grow-1 py-3 ContainerCstyle"
+                                className="flex-grow-1 py-3 ContainerCstyle overflow-auto"
                         >
                                 <Outlet />
                         </Container>
