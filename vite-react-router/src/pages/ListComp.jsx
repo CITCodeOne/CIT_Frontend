d54bac70e1e38df91e11378b1b89193a13bd6dc2
JepMach <jeppea@ruc.dk>
2025-12-09 14:28:03 +0100

    Added data models for titles, individuals and users

--- vite-react-router/src/pages/ListComp.jsx
+++ vite-react-router/src/pages/ListComp.jsx
@@ -3,18 +3,19 @@ import Container from "react-bootstrap/Container";
 import lion from "../pics/lion.jpg";
 import girl from "../pics/girl.jpg";
 import mike from "../pics/mike.jpg";
+import Movie from "../business-logic-layer/data-models/Movie"
 
 //dummy object array
-let size = 200;
+let size = 20;
 let demoItems = [];
 for(let i = 0; i < size; i++){
-    demoItems.push({
-        id: i,
-        name: "Richard Nixon",
-        img: lion,
-        description: "I'm for demoCRAZY!",
-        rating: 7.8
-    })
+    demoItems.push(
+        new Movie({
+            name : "ep"+i,  
+            releaseYear : 1998,
+            directors : ['Mike Hunt', 'That Guy']
+        })
+    )
 }
 
 
@@ -26,15 +27,23 @@ export default function ListPage() {
             itemArray={demoItems}
             renderItem={(item) => (
                 <>
-                    <img src={item.img} alt={item.name} className="img-fluid py-1 px-1 rounded-4 w-25 object-fit-cover overflow-hidden" />
+                    <img src={item.banner} alt={item.name} className="img-fluid py-1 px-1 rounded-4 w-25 object-fit-cover overflow-hidden" />
                     <Container className="overflow-hidden" style={{height: "95%"}}>
-                        <h2>{item.id}</h2>
                         <h3 className="mb-0">{item.name}</h3>
-                        <p className="lh-sm">{item.description}</p>
+                        <p className="lh-sm">{item.plot}</p>
+                        {item.directors && item.directors.length > 0 && (
+                            <Container className="d-flex">
+                                Directors: &nbsp; 
+                                {item.directors.map((dir, index) => (
+                                    <p key={index}>{dir},&nbsp;</p>
+                                ))}
+                            </Container>   
+                        )}
+                        <p>{item.releaseYear}</p>
                     </Container>
 
                     <Container className="bg-warning rounded-end-4" style={{width: "15%"}}>
-                        <p className="text-center fs-1 fst-italic fw-bold py-4">{item.rating}</p>
+                        <p className="text-center fs-1 fst-italic fw-bold py-4 overflow-hidden">{item.rating}</p>
                     </Container>
                 </>
             )}
