1c814bcb15ab0f89f84477d342d9603d659c81c4
JepMach <jeppea@ruc.dk>
2025-12-09 10:25:00 +0100

    Update Home.jsx

--- vite-react-router/src/pages/Home.jsx
+++ vite-react-router/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import lionImage from '../pics/bowling.gif';
+import lionImage from '../pics/lion.gif';
 import SignInOffcanvas from '../components/SignInOffcanvas';
 //Test
 function Home() {
