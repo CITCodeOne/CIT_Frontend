a9d5fe869ee2c9a893986d36a98e6d57400c51af
JepMach <jeppea@ruc.dk>
2025-12-10 14:36:25 +0100

    90% done, text overlaps in small windows

--- vite-react-router/src/components/ListComp.jsx
+++ vite-react-router/src/components/ListComp.jsx
@@ -13,21 +13,19 @@ const chunkItems = (items, size) => {
 };
 
 /*Default size values*/
-    const aH = "88";
-    const aW = "100";
-    const aYP = "0";
-    const aXP = "0";
-    const iH = "120";
+const defaultSizing = {
+    aH: "88",
+    aW: "100",
+    aYP: "0",
+    aXP: "0",
+    iH: "100"
+};
 /* ----------------- */
 
 
 function MakeList({
     itemArray = [],
-    areaHeight = aH,
-    areaWidth = aW,
-    areaYPos = aYP,
-    areaXPos = aXP,
-    itemHeight = iH,
+    listSizing = defaultSizing,
     renderItem
 }) {
     const [itemsListed, setListedItems] = useState([]);
@@ -45,15 +43,15 @@ function MakeList({
         <>
             {items.map((listItems, listIndex) => (
                 <Container className="overflow-auto" style={{
-                    height: areaHeight + "vh",
-                    width: areaWidth + "%",
+                    height: listSizing.aH + "vh",
+                    width: listSizing.aW + "%",
                     position: "relative",
-                    top: areaYPos + "%",
-                    left: areaXPos + "%"
+                    top: listSizing.aYP + "%",
+                    left: listSizing.aXP + "%"
                 }}
                     key={`list-${listIndex}`}>
                     {listItems.map((item, itemIndex) => (
-                        <Container className="d-flex my-2 bg-success rounded-4 px-0" style={{ height: itemHeight + "px" }} key={`item-${listIndex}-${itemIndex}`} >
+                        <Container className="d-flex my-2 bg-success rounded-4 px-0" style={{ height: listSizing.iH + "px" }} key={`item-${listIndex}-${itemIndex}`} >
                             {renderListItem(item, listIndex * listItems + itemIndex)}
                         </Container>
                     ))}
@@ -65,35 +63,32 @@ function MakeList({
 
 export default function ItemList({
     itemsRecieved = [],
-    areaH = aH,
-    areaW = aW,
-    areaYP = aYP,
-    areaXP = aXP,
-    itemH = iH
+    sizeSettings = defaultSizing,
 }) {
     return (
         <MakeList
             itemArray={itemsRecieved}
-            areaHeight={areaH}
-            areaWidth={areaW}
-            areaYPos={areaYP}
-            areaXPos={areaXP}
-            itemHeight={itemH}
+            listSizing={sizeSettings}
             renderItem={(item) => (
                 <>
                     <img src={item.banner} alt={item.name} className="img-fluid py-1 px-1 rounded-4 w-25 object-fit-cover overflow-hidden" />
                     <Container className="overflow-hidden" style={{ height: "95%" }}>
-                        <h3 className="mb-0">{item.name}</h3>
-                        <p className="lh-sm">{item.plot}</p>
-                        {item.directors && item.directors.length > 0 && (
-                            <Container className="d-flex">
-                                Directors: &nbsp;
-                                {item.directors.map((dir, index) => (
-                                    <p key={index}>{dir},&nbsp;</p>
-                                ))}
-                            </Container>
-                        )}
-                        <p>{item.releaseYear}</p>
+                        <h3 className="overflow-hidden mb-0" style={{ height: "40%", whiteSpace: "nowrap" }}>{item.name}</h3>
+                        <p className="overflow-hidden lh-sm" style={{ position: "relative", height: "45%", width: "98%", left: "3%", top: "-6%" }}>
+                            {item.plot}
+                        </p>
+                        <Container className="overflow-hidden" style={{ whiteSpace: "nowrap", width: "70%", position: "relative", top: "-27%", left: "-18%" }}>
+                            {item.directors && item.directors.length > 0 && (
+                                <Container className="d-flex">
+                                    Directors: &nbsp;
+                                    {item.directors.map((dir, index) => (
+                                        <p key={index}>{dir},&nbsp;</p>
+                                    ))}
+                                </Container>
+                            )}
+                        </Container>
+                        <p className="text-end" style={{position: "relative", top: "-67%",}}>Released: {item.releaseYear}</p>
+                        
                     </Container>
                     <Container className="bg-warning rounded-end-4" style={{ width: "15%" }}>
                         <p className="text-center fs-1 fst-italic fw-bold py-4 overflow-hidden">{item.rating}</p>
