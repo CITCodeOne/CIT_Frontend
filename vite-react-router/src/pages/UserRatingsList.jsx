d9d47eab0005e0382b2b369c73ff7a1009953f33
RUCcriss <150340635+RUCcriss@users.noreply.github.com>
2025-12-17 22:41:27 +0100

    Fix double navigation on individual page links
    Updated MainDisplay to avoid wrapping individual items with a Link, preventing double navigation when their children have navigation handlers. Also removed commented-out ListManager modal code from UserRatingsList.

--- vite-react-router/src/pages/UserRatingsList.jsx
+++ vite-react-router/src/pages/UserRatingsList.jsx
@@ -130,10 +130,6 @@ export default function UserRatingsList() {
                     )}
                 />
             )}
-
-            {/* ListManager Modal */}
-            {/* ListManager removed */}
-
             {/* message popup*/}
             {message && (
                 <div
